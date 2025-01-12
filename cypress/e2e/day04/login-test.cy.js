1e5fb27eb24c114ffdbe17550d5d1ea5658c3551
NiyaziPro <niyazicholak@gmail.com>
2025-01-12 16:49:47 +0100

    12.01.2025

--- cypress/e2e/day04/login-test.cy.js
+++ cypress/e2e/day04/login-test.cy.js
@@ -1,20 +1,19 @@
 /// <reference types="cypress"/>
 
-context('Koala Resort Hotel',()=>{
-    it('Log in Test', ()=>{
-        cy.visit('http://64.227.123.49/');
-        cy.get('[data-rr-ui-event-key="5"]').click();
-        cy.get('.user-input').type('nadmin@mail.com');
-        cy.get('.user-password-input').type('Ntester123#');
-        cy.get('.submit-button').click();
-        cy.get('.back-site > .d-none').click();
-        cy.get('[data-rr-ui-event-key="4"]').click();
-        cy.get('#firstName').type('Nyz');
-        cy.get('#lastName').type('Tester');
-        cy.get('#email').type('ntester@mail.com');
-        cy.get('#message').type('Hello, Cypress :)');
-        cy.get('.dynamic-button').click();
-        cy.get('.p-toast-message-text').should('be.visible','Success')
-
-    })
-})
+context("Get Land Estate", () => {
+  it("Log in Test", () => {
+    cy.visit("http://64.227.123.49/");
+    cy.get('[data-rr-ui-event-key="5"]').click();
+    cy.get(".user-input").type("nadmin@mail.com");
+    cy.get(".user-password-input").type("Ntester123#");
+    cy.get(".submit-button").click();
+    cy.get(".back-site > .d-none").click();
+    cy.get('[data-rr-ui-event-key="4"]').click();
+    cy.get("#firstName").type("Nyz");
+    cy.get("#lastName").type("Tester");
+    cy.get("#email").type("ntester@mail.com");
+    cy.get("#message").type("Hello, Cypress :)");
+    cy.get(".dynamic-button").click();
+    cy.get(".p-toast-message-text").should("be.visible", "Success");
+  });
+});
---
+++ cypress/e2e/day05/test01.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+const { faker } = require("@faker-js/faker");
+
+describe("Get Land Estate Test", () => {
+  it("Login Test", () => {
+    cy.visit("http://64.227.123.49/");
+    cy.contains("Register").click();
+    cy.get(":nth-child(1) > .user-input").type(faker.person.firstName());
+    cy.get(":nth-child(2) > .user-input").type(faker.person.lastName());
+    cy.get(":nth-child(3) > .user-input").type(
+      faker.phone.number("##########")
+    );
+    cy.get(":nth-child(4) > .user-input").type(faker.internet.email());
+    const password = 'Tester123#';
+    cy.xpath('//input[@name="password"]').type(password);
+    cy.xpath('//input[@name="confirmPassword"]').type(password);
+    cy.get('#terms').check();
+    cy.get('.form-submit-button').click();
+    cy.get('.p-toast-detail').should('contain.text','Your registration has been completed')
+
+  });
+});
