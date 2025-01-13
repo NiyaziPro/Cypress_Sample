fbab3cdb1dbacb9c7368f378fa22f1fc12009bce
NiyaziPro <niyazicholak@gmail.com>
2025-01-13 19:11:11 +0100

    13.01.2025

--- cypress/e2e/day06.cy.js/Login-test02.cy.js
+++ cypress/e2e/day06.cy.js/Login-test02.cy.js
@@ -1,12 +1,29 @@
 /// <reference types="cypress"/>
 /// <reference types="cypress-xpath"/>
 
-describe('Login Test', () => {
-    it('Test Case 01', () => {
-        cy.visit('http://automationpractice.com/index.php');
-        cy.get('.login').click();
+describe("Login Test", () => {
+  it("Test Case 01", () => {
+    cy.visit("https://allovercommerce.com/");
+    cy.get(".login").click();
 
-        //1.YOL
-        cy.get(':nth-child(1) > label').should('contain','Email address');
-    });
+    //1.YOL
+    cy.get("#signin > .woocommerce-form > :nth-child(1) > label").should(
+      "contain",
+      "email address"
+    );
+
+    //2.YOL
+    cy.get("#signin > .woocommerce-form > :nth-child(1) > label").then(
+      (emailtext) => {
+        expect(emailtext.text()).to.contains("Username or email address");
+      }
+    );
+
+    //3.YOL
+    cy.get("#signin > .woocommerce-form > :nth-child(1) > label")
+      .invoke("text")
+      .then((emailtext2) => {
+        expect(emailtext2).to.contains("Username or email address");
+      });
   });
+});
---
+++ cypress/e2e/day06.cy.js/Login-test03.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress"/>
+/// <reference types="cypress-xpath"/>
+
+import data from "..//..//fixtures/credentials.json";
+
+describe("Login Test ", () => {
+  it("Positive Test", () => {
+    const username = "eshawn.kolden@floodouts.com";
+    const password = "Mikebastian1234";
+
+    cy.visit("https://allovercommerce.com/");
+    cy.get(".login").click();
+
+    cy.get("#username").type(username);
+    cy.get("#password").type(password);
+    cy.get("#signin > .woocommerce-form > .woocommerce-button").click();
+
+    cy.get(".login > span").should("have.text", "Sign Out");
+  });
+
+  it("Positive Test", () => {
+    cy.visit("https://allovercommerce.com/");
+    cy.get(".login").click();
+
+    cy.get("#username").type(data.username);
+    cy.get("#password").type(data.password);
+    cy.get("#signin > .woocommerce-form > .woocommerce-button").click();
+
+    cy.get(".login > span").should("contain", "Sign Out");
+  });
+
+  it.only("Negative Test", () => {
+    const username = "eshawn.kolden@floodouts.co";
+    const password = "Mikebastian12";
+
+    cy.visit("https://allovercommerce.com/");
+    cy.get(".login").click();
+
+    cy.get("#username").type(username);
+    cy.get("#password").type(password);
+    cy.get("#signin > .woocommerce-form > .woocommerce-button").click();
+
+    cy.get("#signin > .woocommerce-form > .submit-status").should(
+      "include.text",
+      "Wrong username or password."
+    );
+  });
+});
